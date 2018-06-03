a30314ff2f4d8116fb63ad5f322bfb308e4ad811
Karanvir Panesar <singh.karanvir21@gmail.com>
2018-06-03 01:53:49 -0700

    Remove extra or commented code
    Also made code formatting more consistent.

--- src/IconArea.cpp
+++ src/IconArea.cpp
@@ -1,5 +1,5 @@
 //
-// Created by karan on 5/3/18.
+// Created by Karanvir Panesar on 5/3/18.
 //
 
 #include <iostream>
@@ -8,36 +8,15 @@
 #include <glibmm/main.h>
 #include "IconArea.h"
 
-IconArea::IconArea()
-        :m_radius(0.42), m_line_width(0.05)
-{
+IconArea::IconArea() : m_radius(0.42), m_line_width(0.025) {
     Glib::signal_timeout().connect(sigc::mem_fun(*this, &IconArea::on_timeout), 10);
-
-    try {
-        image = Gdk::Pixbuf::create_from_file("image.svg");
-    }
-    catch (const Gdk::PixbufError& ex) {
-        std::cerr << "PixbufError: " << ex.what() << std::endl;
-    }
 }
 
-IconArea::~IconArea()
-{
+IconArea::~IconArea() {
 
 }
 
-bool IconArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
-{
-//    Gtk::Allocation allocation = get_allocation();
-//    const int width = allocation.get_width();
-//    const int height = allocation.get_height();
-//
-//    cr->scale(width, height);
-//
-//    Gdk::Cairo::set_source_pixbuf(cr, image, (width - image->get_width())/2, (height - image->get_height())/2);
-//    cr->paint();
-//
-//    return true;
+bool IconArea::on_draw(const Cairo::RefPtr<Cairo::Context> &cr) {
     Gtk::Allocation allocation = get_allocation();
     const int width = allocation.get_width();
     const int height = allocation.get_height();
@@ -50,32 +29,32 @@ bool IconArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
     cr->set_line_width(m_line_width);
 
     cr->save();
-    cr->set_source_rgba(0.337, 0.612, 0.117, 0.9);   // green
+    cr->set_source_rgba(1, 0.95, 0.9, 1);
     cr->paint();
     cr->restore();
     cr->arc(0, 0, m_radius, 0, 2*M_PI);
     cr->save();
-    cr->set_source_rgba(1.0, 1.0, 1.0, 0.8);
+    cr->set_source_rgba(1.0, 1.0, 1.0, 0);
     cr->fill_preserve();
     cr->restore();
     cr->stroke_preserve();
     cr->clip();
 
     //clock ticks
-    for (int i = 0; i<12; i++) {
+    for (int i = 0; i < 12; i++) {
         double inset = 0.05;
 
         cr->save();
-        cr->set_line_cap(Cairo::LINE_CAP_ROUND);
+        cr->set_line_cap(Cairo::LINE_CAP_BUTT);
 
-        if (i%3!=0) {
+        if (i%3 != 0) {
             inset *= 0.8;
-            cr->set_line_width(0.03);
+            cr->set_line_width(m_line_width/2);
         }
 
         cr->move_to(
-                (m_radius-inset)*cos(i*M_PI/6),
-                (m_radius-inset)*sin(i*M_PI/6));
+                (m_radius - inset)*cos(i*M_PI/6),
+                (m_radius - inset)*sin(i*M_PI/6));
         cr->line_to(
                 m_radius*cos(i*M_PI/6),
                 m_radius*sin(i*M_PI/6));
@@ -86,7 +65,7 @@ bool IconArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
     // store the current time
     time_t rawtime;
     time(&rawtime);
-    struct tm* timeinfo = localtime(&rawtime);
+    struct tm *timeinfo = localtime(&rawtime);
 
     // compute the angles of the indicators of our clock
     double minutes = timeinfo->tm_min*M_PI/30;
@@ -102,22 +81,22 @@ bool IconArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
     cr->set_source_rgba(0.7, 0.7, 0.7, 0.8); // gray
     cr->move_to(0, 0);
     cr->line_to(sin(seconds)*(m_radius*0.9),
-            -cos(seconds)*(m_radius*0.9));
+                -cos(seconds)*(m_radius*0.9));
     cr->stroke();
     cr->restore();
 
     // draw the minutes hand
     cr->set_source_rgba(0.117, 0.337, 0.612, 0.9);   // blue
     cr->move_to(0, 0);
-    cr->line_to(sin(minutes+seconds/60)*(m_radius*0.8),
-            -cos(minutes+seconds/60)*(m_radius*0.8));
+    cr->line_to(sin(minutes + seconds/60)*(m_radius*0.8),
+                -cos(minutes + seconds/60)*(m_radius*0.8));
     cr->stroke();
 
     // draw the hours hand
     cr->set_source_rgba(0.337, 0.612, 0.117, 0.9);   // green
     cr->move_to(0, 0);
-    cr->line_to(sin(hours+minutes/12.0)*(m_radius*0.5),
-            -cos(hours+minutes/12.0)*(m_radius*0.5));
+    cr->line_to(sin(hours + minutes/12.0)*(m_radius*0.5),
+                -cos(hours + minutes/12.0)*(m_radius*0.5));
     cr->stroke();
     cr->restore();
 
@@ -128,13 +107,12 @@ bool IconArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
     return true;
 }
 
-bool IconArea::on_timeout()
-{
+bool IconArea::on_timeout() {
     // force our program to redraw the entire clock.
     auto win = get_window();
     if (win) {
         Gdk::Rectangle r(0, 0, get_allocation().get_width(),
-                get_allocation().get_height());
+                         get_allocation().get_height());
         win->invalidate_rect(r, false);
     }
     return true;
